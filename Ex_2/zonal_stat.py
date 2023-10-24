e5aa5529e48b80e001d15deba87e74069b9d7093
IUrdi <148715247+IUrdi@users.noreply.github.com>
2023-10-24 16:34:16 +0200

    Add files via upload

---
+++ Ex_2/zonal_stat.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+
+def read_data(fname: str, tipo: type) -> np.ndarray:
+    """
+
+    Examples:
+    --------
+    >>> read_data('./datos/zonas.txt',tipo=np.int_)
+    array([[1, 1, 1, 1, 3, 3],
+           [1, 1, 1, 1, 3, 1],
+           [2, 2, 3, 3, 3, 4],
+           [2, 2, 3, 3, 3, 4],
+           [2, 2, 3, 3, 2, 2],
+           [3, 3, 3, 3, 3, 2]])
+    >>> read_data('./datos/valores.txt',tipo=np.float_)
+    array([[5., 3., 4., 4., 4., 2.],
+           [2., 1., 4., 2., 6., 3.],
+           [8., 4., 3., 5., 3., 1.],
+           [4., 2., 4., 3., 2., 2.],
+           [6., 3., 3., 7., 4., 2.],
+           [5., 5., 2., 3., 1., 3.]])
+    >>> read_data('inexistentfile.txt',tipo=np.int_)
+    Traceback (most recent call last):
+        ...
+    FileNotFoundError: Input file inexistentfile.txt not found in local directory
+    """
+    
+    # Escribe aquí tu código
+    # No olvides documentar la función
+    try:
+        return np.loadtxt(fname,dtype=tipo)
+    except FileNotFoundError:
+        raise FileNotFoundError(f'Input file {fname} not found in local directory')
+        
+
+def set_of_areas(zonas: np.ndarray)-> set[int]:
+    """
+
+    Examples:
+    --------
+    >>> set_of_areas(np.arange(10).reshape(5, 2))
+    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+    >>> set_of_areas(np.zeros(10, dtype=np.int_).reshape(5, 2))
+    {0}
+    >>> set_of_areas(np.array([2, 3, 4, 2, 3, 4], dtype=np.int_).reshape(3, 2))
+    {2, 3, 4}
+    >>> set_of_areas(np.zeros(3, dtype=np.float_))
+    Traceback (most recent call last):
+        ...
+    TypeError: The elements type must be int, not float64
+    """
+    # Escribe aquí tu código
+    # No olvides documentar la función
+    if zonas.dtype!='int':
+        raise TypeError("The elements type must be int, not {}".format(zonas.dtype))
+    return set(zonas.flatten())
+
+
+def mean_areas(zonas: np.ndarray,valores: np.ndarray)-> np.ndarray:
+    """
+
+    Examples:
+    --------
+    >>> mean_areas(np.array([[1, 1, 1, 1, 3, 3],[1, 1, 1, 1, 3, 1]]),np.array([[5., 3., 4., 4., 4., 2.],[2., 1., 4., 2., 6., 3.]]))
+    array([[3.1, 3.1, 3.1, 3.1, 4. , 4. ],
+           [3.1, 3.1, 3.1, 3.1, 4. , 3.1]])
+    >>> mean_areas(np.array([[2,3], [4,5]]), np.array([[1,2,3], [4,5,6]]))  # dimensiones distintas
+    Traceback (most recent call last):
+        ...
+    IndexError: Shape of zonas and valores must be the same. zonas: (2, 2) != valores: (2, 3)
+    """
+
+    '''zonas=read_data('./datos/zonas.txt',tipo=np.int_)
+    valores=read_data('./datos/valores.txt',tipo=np.float_)'''
+    # Check input array dimensions
+    if zonas.shape != valores.shape:
+        raise IndexError(f'Shape of zonas and valores must be the same. zonas: {zonas.shape} != valores: {valores.shape}')
+    conjunto=set(zonas.flatten())
+    resMean=np.copy(valores)
+    for zona in conjunto:
+        mask = (zonas==zona)
+        resMean[mask]=valores[mask].mean().round(1)
+    return resMean
+    # Escribe aquí el código de la función mean_areas
+    # No olvides documentar la función y escribir las anotaciones de tipos
+    # Añade más ejemplos para doctest
+
+
+
+# ------------ test  --------#
+import doctest
+
+def test_doc()-> None:
+    """
+    The following instructions are to execute the tests of same functions
+    If any test is fail, we will receive the notice when executing
+    :return: None
+    """
+    doctest.run_docstring_examples(read_data, globals(), verbose=False)  # vemos los resultados de los test que fallan
+    doctest.run_docstring_examples(set_of_areas, globals(), verbose=False)  # vemos los resultados de los test que fallan
+    doctest.run_docstring_examples(mean_areas, globals(), verbose=False)  # vemos los resultados de los test que fallan
+
+
+if __name__ == "__main__":
+    test_doc()   # executing tests
